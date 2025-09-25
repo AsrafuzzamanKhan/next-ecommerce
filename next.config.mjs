849239277003bac97b9891d631eef3f93c3ad4e9
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2025-09-25 21:58:29 +0930

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                pathname: '**',
-            },
-        ],
-    },
-};
+// /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
